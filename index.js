b3c6f777293fee26135c8f112ea68ab5521fd95f
Austin Godber <godber@uberhip.com>
2016-10-24 17:07:44 -0700

    Extracted draw and request functionality into modules
    Mostly as an exercise I have extracted the text UI draw and the API
    request functionality out into separate modules.  I get the feeling
    that when I redo this with promises it’s not going to make sense to
    split this up as much.

--- index.js
+++ index.js
@@ -2,10 +2,8 @@
 
 'use strict';
 
-const request = require('request');
-const Jetty = require('jetty');
-
-const jetty = new Jetty(process.stdout);
+const request = require('./lib/request');
+const draw = require('./lib/draw');
 
 const argv = require('yargs')
   .usage(`Usage: $0 [options] [host]`)
@@ -60,59 +58,6 @@ const sections = {
 };
 
 /**
- * Generates a Header
- * @param {string} title Short string representing title of section header.
- * @param {string} url URL of the API endpoint
- * @return {string} s The formatted string to be printed to title.
- */
-function makeHeader(title, url) {
-  return `
---------------------------------------------------------------------------------
-  ${title} (${url})
---------------------------------------------------------------------------------
-
-`;
-}
-
-/**
- * Draws the whole screen
- */
-function drawScreen() {
-  jetty
-    .clear()
-    .moveTo([0, 0]);
-
-  let date = new Date();
-  for (let section in sections) {
-    if (sections.hasOwnProperty(section)) {
-      let url = baseUrl + sections[section].endpoint;
-      process.stdout.write(makeHeader(section, url));
-      process.stdout.write(sections[section].value);
-    }
-  }
-  process.stdout.write(`\nUpdated at: ${date}\n`);
-}
-
-/**
- * Retrieves data from the specified endpoint API URL
- * @param  {string} section Which section are we updating
- * @param {getEndpointCallback} callback
- */
-function getEndpoint(section, callback) {
-  let url = baseUrl + sections[section].endpoint;
-  let newUrl = url + `?size=8`;  // limit number of responses
-
-  request(newUrl, function(error, response, body) {
-    if (!error && response.statusCode == 200) {
-      callback(null, section, body);
-    } else {
-      callback(error, section);
-    }
-  });
-};
-
-
-/**
  * Sets the value of the corresponding section to the response body
  * @param {string} error The error returned from the request
  * @param {string} section The section the request came from
@@ -126,15 +71,18 @@ function setValue(error, section, body) {
   } else {
     sections[section].value = body;
   }
-  drawScreen();
+  // draw(sections, genUrl(section));
+  draw(sections);
 };
 
 // Hit all of the endpoints at the requested interval
 for (let section in sections) {
   if (sections.hasOwnProperty(section)) {
-    getEndpoint(section, setValue);  // hit endpoints once for first run
+    let url = baseUrl + sections[section].endpoint;
+
+    request(section, url, setValue);  // once for first run
     setInterval(function() {
-      getEndpoint(section, setValue);
+      request(section, url, setValue);
     }, requestInterval);   // hit endpoints at interval
   }
 }
